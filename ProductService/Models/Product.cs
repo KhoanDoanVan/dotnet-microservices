c2ba5d68004821efca5e618af40d3ecf5ffd3c14
khoanisdev <doanvankhoan124@gmail.com>
2025-11-04 15:31:24 +0700

    [feat] create ProductService/Models/Product.cs

---
+++ ProductService/Models/Product.cs
@@ -0,0 +1,15 @@
+namespace ProductService.Models;
+
+
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public int? CategoryId { get; set; }
+    public int? SupplierId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public string? Barcode { get; set; }
+    public decimal Price { get; set; }
+    public string Unit { get; set; } = "pcs";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
