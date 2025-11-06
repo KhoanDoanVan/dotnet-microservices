b5aba0a9f6c55374329d78d6e3ec69c555b2f88e
khoanisdev <doanvankhoan124@gmail.com>
2025-11-06 09:51:41 +0700

    [feat] create ProductService/Models/Supplier.cs,ProductService/DTOs/SupplierDto.cs

---
+++ ProductService/DTOs/SupplierDto.cs
@@ -0,0 +1,27 @@
+namespace ProductService.DTOs;
+
+
+public class SupplierDto
+{
+    public int SupplierId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+}
+
+public class CreateSupplierRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+}
+
+public class UpdateSupplierRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+}
---
+++ ProductService/Models/Supplier.cs
@@ -0,0 +1,11 @@
+namespace ProductService.Models;
+
+
+public class Supplier
+{
+    public int SupplierId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+}
